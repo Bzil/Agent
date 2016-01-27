4b3b75b314e415573e5bc41e6c270b77e147dbd3
Bzil <bzchap@gmail.com>
2016-01-27 12:30:28 +0100

    [ FEAT     ] : Add syso pattern package.func(params)

--- src/main/java/bz/pock/agent/config/LogAgent.java
+++ src/main/java/bz/pock/agent/config/LogAgent.java
@@ -2,19 +2,16 @@ package bz.pock.agent.config;
 
 import bz.pock.agent.interceptor.LoggerInterceptor;
 import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.description.NamedElement;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.SuperMethodCall;
-import net.bytebuddy.matcher.ElementMatcher;
 
 import java.lang.instrument.Instrumentation;
 
+import static net.bytebuddy.matcher.ElementMatchers.any;
 import static net.bytebuddy.matcher.ElementMatchers.isAnnotatedWith;
-import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
 import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.not;
 
 public class LogAgent {
 
@@ -23,16 +20,11 @@ public class LogAgent {
     public static void premain(String agentArguments, Instrumentation instrumentation) {
         System.out.println("Hello World from Agent");
 
-        new AgentBuilder.Default().withListener(new AgentListener()).type(geTypeMatchers())
+        new AgentBuilder.Default().withListener(new AgentListener()).type(any())
                 .transform((builder, typeDescription) -> builder
                         .invokable(isAnnotatedWith(named(LOG_ANNOTATION)))
-                        .intercept(SuperMethodCall.INSTANCE.andThen(MethodDelegation.to(LoggerInterceptor.class)))
+                        .intercept(MethodDelegation.to(LoggerInterceptor.class).andThen(SuperMethodCall.INSTANCE))
                 ).installOn(instrumentation);
-    }
-
-    private static ElementMatcher.Junction<NamedElement> geTypeMatchers() {
-        return not(nameStartsWith("java").or(nameStartsWith("sun")).or(nameStartsWith("javax"))
-                .or(nameStartsWith("com.sun")));
     }
 }
 
@@ -44,8 +36,8 @@ class AgentListener implements AgentBuilder.Listener {
 
     @Override
     public void onError(String typeName, Throwable throwable) {
-        // System.err.println("Error for " + typeName);
-        // throwable.printStackTrace();
+         System.err.println("Error for " + typeName);
+         throwable.printStackTrace();
     }
 
     @Override
--- src/main/java/bz/pock/agent/interceptor/LoggerInterceptor.java
+++ src/main/java/bz/pock/agent/interceptor/LoggerInterceptor.java
@@ -2,18 +2,20 @@ package bz.pock.agent.interceptor;
 
 import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import net.bytebuddy.implementation.bind.annotation.This;
-import net.bytebuddy.jar.asm.commons.Method;
 
-import java.util.Arrays;
-
+import java.lang.reflect.Method;
+import java.util.StringJoiner;
 
 public class LoggerInterceptor {
-    public void log(@This Object currentInstance, @AllArguments Object[] allArguments,
-                    @Origin Method method) {
-        StringBuilder logs = new StringBuilder(currentInstance.getClass().getCanonicalName())
-                .append(method.getName()).append("(");
-        Arrays.stream(allArguments).map(o -> logs.append(o.toString()).append(", "));
-        System.out.println(logs.append(")").toString());
+    @RuntimeType
+    public static void log(@This Object currentInstance, @AllArguments Object[] allArguments, @Origin Method method) {
+        StringBuilder logs = new StringBuilder(currentInstance.getClass().getCanonicalName()).append(".").append(method.getName()).append("(");
+        StringJoiner joiner = new StringJoiner(",");
+        for (Object o : allArguments) {
+            joiner.add(o.toString());
+        }
+        System.out.println(logs.append(joiner.toString()).append(")").toString());
     }
 }
